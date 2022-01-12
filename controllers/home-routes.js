beb00fde0ace695d85ef6265b40400285fd2ee65
Kassidy R <kassidy.ralph@gmail.com>
2022-01-12 15:33:33 -0500

    created login/signup page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: ["id", "post_content", "title", "created_at"],
     include: [
@@ -24,13 +25,22 @@ router.get("/", (req, res) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
 
       res.render("homepage", {
-        posts
+        posts,
       });
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
