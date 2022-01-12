9190cedfd0c6105946b2666a04662434e9efe67f
Kassidy R <kassidy.ralph@gmail.com>
2022-01-11 22:20:12 -0500

    created homepage template

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,12 +1,25 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_content", "title", "created_at"],
-    order:[['created at', 'DESC']],
+    order: [["created_at", "DESC"]],
+    attributes: [
+      "id",
+      "post_content",
+      "title",
+      "created_at",
+    ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
