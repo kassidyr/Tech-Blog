8bdf022d79dd02d38fa841b48ac4db7d2617d9c0
Kassidy R <kassidy.ralph@gmail.com>
2022-01-13 09:57:01 -0500

    added date and plural helpers

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
 const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "Super secret secret",
   cookie: {},
